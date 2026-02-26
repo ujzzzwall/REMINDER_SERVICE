415cbf066201719f7dea16228c3c4ce9b0634739
Ujjwal singh <ujzzzwall@gmail.com>
2026-02-26 15:57:13 +0530

    added messageQueue

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const bodyParser =  require('body-parser')
 const {PORT} = require('./config/serverConfig')
+
+
 const jobs = require('./utils/job');
 
 // const {sendBasicEmail} = require('./services/EmailService')
 const ticketController = require('./controller/Email-controller')
-
-const setupAndStartServer = ()=>{
+const {subscribeMessage , createChannel} = require('./utils/messageQueue')
+const {REMINDER_BINDING_KEY} = require('./config/serverConfig')
+const setupAndStartServer = async()=>{
 
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
-  
+
   app.post('/api/v1/tickets' ,ticketController.create);
   app.delete('/api/v1/tickets/:id' ,ticketController.destroy);
+
+  const channel = await createChannel();
+  subscribeMessage(channel,undefined,REMINDER_BINDING_KEY)
 
   app.listen(PORT , ()=>{
     console.log(`server started at port : ${PORT}`)
